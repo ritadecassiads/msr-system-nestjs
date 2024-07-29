718d39042492d18161f7500ed2c12cdf428465ab
Rita de Cassia Damasceno Santos <rita.santos@MJV.net>
2024-07-29 16:57:02 -0300

    add cors]

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Configuração do CORS
+    app.enableCors({
+      origin: 'http://localhost:4200', // Permitir apenas essa origem
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+      allowedHeaders: 'Content-Type, Accept, Authorization', // Cabeçalhos permitidos
+    });
+    
   await app.listen(3000);
   console.log('Servidor rodando na porta 3000');
 }
