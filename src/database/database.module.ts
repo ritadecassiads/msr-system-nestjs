c96e324a96ed1879ab2cf1f807dc23d331b3c407
ritadecassiads <ritadamascenosantos3@gmail.com>
2025-04-10 22:07:38 -0300

    ajuste para chamar cluster

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/msr-system', {
-      connectionFactory: (connection) => {
-        connection.on('connected', () => {
-          console.log('Conectado com o MongoDB!');
-        });
-        connection.on('error', (error: any) => {
-          console.error('Erro na conexão com o MongoDB:', error);
-        });
-        return connection;
-      },
+    ConfigModule.forRoot(), // Carrega as variáveis de ambiente do .env
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URI'), // Pega a URI do banco do .env
+        connectionFactory: (connection) => {
+          connection.on('connected', () => {
+            console.log('Conectado com o MongoDB!');
+          });
+          connection.on('error', (error: any) => {
+            console.error('Erro na conexão com o MongoDB:', error);
+          });
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
