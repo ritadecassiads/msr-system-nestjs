82cbd6cd4ea5c0f627028e66550f5c45c181cf03
ritadecassiads <ritadamascenosantos3@gmail.com>
2025-04-22 19:19:55 -0300

    ajuste para deploy

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const seedService = app.get(SeedService);
   await seedService.seed();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('Servidor rodando na porta 3000');
 }
 bootstrap();
